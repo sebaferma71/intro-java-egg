86a4d0a25a47757f04efa392ab8f66d266f8bbef
Sebastian Fermanelli <sebastianfermanelli@gmail.com>
2023-03-16 21:37:57 -0300

    Encuentro 2

--- guia2/ejercicio2.java
+++ guia2/ejercicio2.java
@@ -1,5 +1,15 @@
 package guia2;
 
+import java.util.Scanner;
+
 public class ejercicio2 {
     
+    public static void main(String[] args) {
+        try (Scanner ingresar = new Scanner(System.in)) {
+            System.out.println("Ingrese su nombre:");
+            String nombre = ingresar.nextLine();
+
+            System.out.println("Hola, " + nombre);
+        }
+    }
 }
--- guia2/ejercicio3.java
+++ guia2/ejercicio3.java
@@ -1,5 +1,18 @@
 package guia2;
 
+import java.util.Scanner;
+
 public class ejercicio3 {
     
+    public static void main(String[] args) {
+        try(Scanner ingresar = new Scanner(System.in)) {
+            System.out.println("Ingrese una frase:");
+            String frase = ingresar.nextLine();
+
+            System.out.println("Frase en mayusculas:");
+            System.out.println(frase.toUpperCase());
+            System.out.println("Frase en minusculas:");
+            System.out.println(frase.toLowerCase());
+        }
+    }
 }
