a3af080522dc05566597a44ae8fc180efbbee749
Sebastian Fermanelli <sebastianfermanelli@gmail.com>
2023-03-23 18:47:38 -0300

    Encuentro 7

--- guia4/ejercicio4.java
+++ guia4/ejercicio4.java
@@ -5,9 +5,36 @@ import java.util.Scanner;
 public class ejercicio4 {
     
     public static void main(String[] args) {
+        int numero;
+
         try (Scanner leer = new Scanner(System.in)) {
-            System.out.println("Ingrese la longitud de cada lado del cuadrado:");
-            a = leer.nextInt();
+            System.out.println("Ingrese un numero para saber si es primo o no:");
+            numero = leer.nextInt();
+
+            if (EsPrimo(numero)) {
+                System.out.println("El numero " + numero + " es primo");
+            }
+            else {
+                System.out.println("El numero " + numero + " no es primo");
+            }
+        }
+    }
+
+    public static boolean EsPrimo(int  num) {
+        int cont;
+
+        cont = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                cont++;
+            }
+        }
+
+        if (cont == 2) {
+            return true;
+        }
+        else {
+            return false;
         }
     }
 }
